365dd68a59bdaba9d3e8c4bb98bdd6400da0d9ee
Erce Ozturk <2329415o@student.gla.ac.uk>
2019-02-06 19:40:46 +0000

    Chapter 9 Exercises

--- rango/views.py
+++ rango/views.py
@@ -207,7 +207,7 @@ def user_login(request):
         else:
             # Bad login details were provided. So we can't log the user in.
             print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
+            return render(request, 'rango/login.html', {'error_message':'Invalid username and/or password entered. Try again'})
     
     # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
@@ -218,7 +218,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 # Use the login_required() decorator to ensure only those logged in can
 # access the view.
